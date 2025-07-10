582a63cbe276fe1f1d2014a992b39443f64e4d41
Igor Gladkikh <ig.gladkikh@gmail.com>
2025-07-10 09:20:29 +0300

    Добавить информацию об используемой памяти

--- src/main/java/com/igladkikh/Main.java
+++ src/main/java/com/igladkikh/Main.java
@@ -24,15 +24,24 @@ public class Main {
         }
         String inputFilePath = args[0];
 
-        SoftReference<List<String>> lines =
-                new SoftReference<>(FileUtil.loadUniqueLinesFromFile(inputFilePath));
-        System.out.printf("Данные из файла: '%s' загружены за %d мс.\n", inputFilePath,
-                Instant.now().toEpochMilli() - appStartTime);
+        SoftReference<List<String>> lines = new SoftReference<>(FileUtil.loadUniqueLinesFromFile(inputFilePath));
+        System.out.printf("Данные из файла: '%s' загружены за %d мс.\n", inputFilePath, Instant.now().toEpochMilli() - appStartTime);
 
         service.add(Objects.requireNonNull(lines.get()));
         FileUtil.writeResultToFile(service.getGroups());
 
         System.out.printf("Результат записан в файл: %s\n", FileUtil.OUTPUT_FILE_NAME);
-        System.out.printf("Время работы программы: %d мс", Instant.now().toEpochMilli() - appStartTime);
+        System.out.printf("Время работы программы: %d мс\n", Instant.now().toEpochMilli() - appStartTime);
+
+        Runtime runtime = Runtime.getRuntime();
+        // Объём памяти, доступный JVM в данное время
+        long totalMemory = runtime.totalMemory();
+        System.out.printf("Объём памяти, доступный JVM: %d Mb\n", totalMemory / 1024 / 1024);
+        // Количество памяти JVM, свободное от занятых объектами
+        long freeMemory = runtime.freeMemory();
+        System.out.printf("Свободный объём памяти: %d Mb\n", freeMemory / 1024 / 1024);
+        // Объём памяти, которую занимают объекты
+        long usedMemory = totalMemory - freeMemory;
+        System.out.printf("Используемая память: %d Mb\n", usedMemory / 1024 / 1024);
     }
 }
